15c0cb009f6ad83e8261e5b4e028537be09f89cf
pamef.2707 <pamef.2707@gmail.com>
2012-09-26 05:16:52 +0000

    Creacion de Activities

---
+++ tdam-2012-g8/src/com/tdam2012/grupo8/ui/CallActivity.java
@@ -0,0 +1,15 @@
+package com.tdam2012.grupo8.ui;
+
+import com.tdam2012.grupo8.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CallActivity extends Activity 
+{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+
+}
---
+++ tdam-2012-g8/src/com/tdam2012/grupo8/ui/CallKeyboardActivity.java
@@ -0,0 +1,37 @@
+package com.tdam2012.grupo8.ui;
+
+
+import com.tdam2012.grupo8.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+
+public class CallKeyboardActivity extends Activity 
+
+{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_keyboard);
+        Button bot = (Button)findViewById(R.id.imageButton1);
+    	bot.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v)
+        	{
+        		Intent i = new Intent(CallKeyboardActivity.this,  CallActivity.class);
+        		startActivity(i);
+        		
+        	}
+    		
+    	}); 	  	
+    }
+	 public boolean onCreateOptionsMenu(Menu menu) {
+	       getMenuInflater().inflate(R.menu.activity_main, menu);
+	       return true;
+	   }
+}
+
+
